d70be019da1ba096805be6fac275300129a05fd8
NazifaPushon <mashiat.pushon@gmail.com>
2021-12-03 22:58:00 +0600

    updating main card

--- src/Pages/Utilities/MainCard.js
+++ src/Pages/Utilities/MainCard.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-const MainCard = ({ img, children }) => (
-    <div className="bg-white  border  my-5 border-gray rounded-xl overflow-hidden">
+const MainCard = ({ img, additionalStyle, children }) => (
+    <div
+        className={`${additionalStyle} bg-white  border  my-5 border-gray rounded-xl overflow-hidden`}
+    >
         {img && <img src={img} alt="" />}
         <div className="p-5">{children}</div>
     </div>
