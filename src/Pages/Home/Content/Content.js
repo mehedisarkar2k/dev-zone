7fba26c6d8e529f0429ddc1a382c0b6672f4ea1a
Ariful Islam <arifulislam161024@gmail.com>
2021-12-03 11:06:21 +0600

    home page layout

---
+++ src/Pages/Home/Content/Content.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Content = () => (
+    <div>
+        <h1>This is content</h1>
+    </div>
+);
+
+export default Content;
---
+++ src/Pages/Home/RightNav/RightNav.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RightNav = () => (
+    <div>
+        <h1>This is right content</h1>
+    </div>
+);
+
+export default RightNav;
